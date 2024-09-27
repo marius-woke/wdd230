c818d2a954689a939605ebccb6c7a8793a5543bc
marius-woke <wok22002@byui.edu>
2024-09-26 18:54:49 -0700

    W02 Team Activity

--- week_01/scripts/getDates.js
+++ week_01/scripts/getDates.js
@@ -1,2 +1,11 @@
 document.getElementById('currentYear').textContent = new Date().getFullYear();
 document.getElementById('modifiedDate').textContent = document.lastModified;
+document.addEventListener('DOMContentLoaded', function () {
+    const hamMenu = document.querySelector('.ham-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamMenu.addEventListener('click', function () {
+        navLinks.classList.toggle('show');
+        hamMenu.classList.toggle('active');
+    });
+});
