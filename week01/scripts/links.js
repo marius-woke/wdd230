f305a53500cdd0867906bb9b2231f4cc885dac9e
marius-woke <wok22002@byui.edu>
2024-10-18 17:41:37 -0700

    lpl

--- week01/scripts/links.js
+++ week01/scripts/links.js
@@ -1,5 +1,5 @@
 const baseURL = "https://marius-woke.github.io/wdd230/"; // Replace with your actual GitHub URL
-const linksURL = `https://marius-woke.github.io/wdd230/week01/data/links.json`;
+const linksURL = `https://marius-woke.github.io/wdd230/week01/data/link.json`;
 
 async function getLinks() {
     try {
